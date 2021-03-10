5b4d68dde3acc0bd061576a13db6bae7b7717c6c
Habib Fauzan Akbar <habibakbarfauzan@gmail.com>
2021-03-10 16:01:38 +0700

    Membuat layout untuk list

---
+++ app/src/main/java/org/d3if4304/galerihewan/Hewan.kt
@@ -0,0 +1,7 @@
+package org.d3if4304.galerihewan
+
+data class Hewan(
+    val Nama : String,
+    val NamaLatin : String,
+    val Jenis : String
+)
--- app/src/main/java/org/d3if4304/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if4304/galerihewan/MainActivity.kt
@@ -2,10 +2,29 @@ package org.d3if4304.galerihewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity","Jumlah Data : " + getData().size)
+    }
+
+    private fun getData(): List<Hewan> {
+        return listOf(
+                Hewan("Angsa", "Cygnus olor","Unggas"),
+                Hewan("Ayam", "Gallus gallus","Unggas"),
+                Hewan("Bebek", "Cairina moschata","Unggas"),
+                Hewan("Domba", "Ovis ammon","Mamalia"),
+                Hewan("Kalkun", "Meleagris gallopavo","Unggas"),
+                Hewan("Kambing", "Capricornis sumatrensis","Mamalia"),
+                Hewan("Kelinci", "Oryctolagus cuniculus","Mamalia"),
+                Hewan("Kerbau", "Bubalus bubalis","Mamalia"),
+                Hewan("Kuda", "Equus caballus","Mamalia"),
+                Hewan("Sapi", "Bos taurus","Mamalia"),
+
+                )
     }
 }
