eba8469fc1280558cbe2b9f06b5649c4be62b78c
Habib Fauzan Akbar <habibakbarfauzan@gmail.com>
2021-05-05 18:57:44 +0700

    3.2.Membuat view model untuk fitur main

--- app/src/main/java/org/d3if4304/galerihewan/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if4304/galerihewan/ui/main/MainAdapter.kt
@@ -6,8 +6,15 @@ import androidx.recyclerview.widget.RecyclerView
 import org.d3if4304.galerihewan.model.Hewan
 import org.d3if4304.galerihewan.databinding.ListItemBinding
 
-class MainAdapter(private val data: List<Hewan>):
-    RecyclerView.Adapter<MainAdapter.ViewHolder>(){
+class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    private val data = mutableListOf<Hewan>()
+    fun updateData(newData: List<Hewan>) {
+        data.clear()
+        data.addAll(newData)
+        notifyDataSetChanged()
+    }
+
 
     class ViewHolder(private val binding: ListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
